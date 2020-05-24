47365389d597104898abd1b85ec72c1dc5da93be
Maxim Kocheshkov <gror_godfroy@mail.ru>
2020-05-24 17:01:19 +0300

    задание 1 - версия 1

---
+++ Phone_contact.py
@@ -0,0 +1,19 @@
+
+
+class Contact:
+
+    def __init__(self, name, surname, phone_number, selected_contact=False, **additionally):
+        self.name = name
+        self.surname = surname
+        self.phone_number = phone_number
+        self.selected_contact = selected_contact
+        self.additionally = additionally
+
+    def __str__(self):
+        return f' Имя: {self.name}\n Фамилия: {self.surname}\n Телефон: {self.phone_number}\n В избранных: ' \
+               f'{self.selected_contact}\n Дополнительная информация:\n \t telegram= ' \
+               f'{self.additionally.setdefault("telegram")}\n \t email= {self.additionally.setdefault("email")}'
+
+
+jhon = Contact('Jhon', 'Smith', '+71234567809', selected_contact=True, telegram='@jhony', email='jhony@smith.com')
+print(jhon)
